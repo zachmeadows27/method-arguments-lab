b8e354dd8052277d5706be7fb5e89eb6c7599e70
zachmeadows27 <73083055+zachmeadows27@users.noreply.github.com>
2020-10-20 17:55:45 -0400

    Update introduction.rb

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language
+def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I speak #{language}."
-language = "English"
 end
 
