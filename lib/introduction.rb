6849353ec309ca64087fa2dc47ecdaecfd6314de
Zach Meadows <zlmeadow@syr.edu>
2020-10-21 17:14:49 -0400

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def introduction(name)
+def introduction("Zach")
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I speak #{language}."
+def introduction_with_language(Zach , Ruby)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
